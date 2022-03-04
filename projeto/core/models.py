c0c14d7c6f2275ad40a23b73f3df7eb19ace9041
Atila Capovilla <capovillaatila@gmail.com>
2022-03-04 17:04:08 -0300

    Criada a app estoque

--- projeto/core/models.py
+++ projeto/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(
-        'craido em',
+        'criado em',
         auto_now_add=True,
         auto_now=False
     )
--- projeto/settings.py
+++ projeto/settings.py
@@ -26,8 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'projeto.core',
     'projeto.produto',
+    'projeto.estoque',
 ]
 
 MIDDLEWARE = [
