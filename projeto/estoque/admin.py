b22c8f9853b957fbe00c95182834ac602c4e8963
Atila Capovilla <capovillaatila@gmail.com>
2022-03-07 15:52:12 -0300

    Criado dados de produtos

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Estoque, EstoqueItens
+
+class EstoqueItensInLine(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInLine,)
     list_display = (
         '__str__',
         'movimento',
--- projeto/produto/admin.py
+++ projeto/produto/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Produto
 
-# admin.site.register(Produto)
+#admin.site.register(Produto)
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
