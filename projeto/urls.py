c7ed4ed3a2bc4d14174957a5cff822d3cf3c4a01
Atila Capovilla <capovillaatila@gmail.com>
2022-03-11 18:30:22 -0300

    Entrada estoque_entrada_list.html

--- projeto/urls.py
+++ projeto/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projeto.core.urls')),
     path('produto/', include('projeto.produto.urls')),
+    path('estoque/', include('projeto.estoque.urls')),
 ]
