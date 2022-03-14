4a1d853a81ec5bda3ddcf3088ff47504f8411baa
Atila Capovilla <capovillaatila@gmail.com>
2022-03-14 17:30:00 -0300

    Entrada estoque_entrada_detail.html

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -4,6 +4,13 @@ from .models import Estoque
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
     objects = Estoque.objects.filter(movimento='e')
-    print(objects)
     context = {'object_list': objects}
     return render(request, template_name, context)
+
+
+def estoque_entrada_detail(request, pk):
+    template_name = 'estoque_entrada_detail.html'
+    obj = Estoque.objects.get(pk=pk)
+    context = {'object': obj}
+    return render(request, template_name, context)
+
