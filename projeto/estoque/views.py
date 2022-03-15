ee95d83525033dc4062b3c2213345c2fe84e60d1
Atila Capovilla <capovillaatila@gmail.com>
2022-03-15 18:28:50 -0300

    Entrada estoque_entrada_form

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from .models import Estoque
+from sys import prefix
+from django.forms import inlineformset_factory
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, resolve_url
+
+from projeto.estoque.forms import EstoqueForm, EstoqueItensForm
+from .models import Estoque, EstoqueItens
 
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
@@ -12,5 +17,39 @@ def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
     obj = Estoque.objects.get(pk=pk)
     context = {'object': obj}
+    return render(request, template_name, context)
+
+
+def estoque_entrada_add(request):
+    template_name = 'estoque_entrada_form.html'
+    estoque_form = Estoque()
+    item_estoque_formset = inlineformset_factory(
+        Estoque,
+        EstoqueItens,
+        form=EstoqueItensForm, 
+        extra=0,
+        min_num=1,
+        validate_min=True,
+    )
+    if request.method=='POST':
+        form =  EstoqueForm(request.POST, instance=estoque_form, prefix='main')
+        formset = item_estoque_formset(
+            request.POST,
+            instance=estoque_form, 
+            prefix='estoque'
+        )
+        if form.is_valid() and formset.is_valid():
+            obj = form.save()
+            formset.save()
+            url = 'estoque:estoque_entrada_detail'
+            return HttpResponseRedirect(resolve_url(url, obj.pk))
+    else:
+        form = EstoqueForm(instance=estoque_form, prefix='main')
+        formset = item_estoque_formset(instance=estoque_form, prefix='estoque')
+
+    context = {
+        'form': form,
+        'formset': formset
+    }
     return render(request, template_name, context)
 
