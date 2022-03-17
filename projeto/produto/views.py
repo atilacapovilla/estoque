ef06b256ccc31d45f4a1cc26450fde9fe70028e5
Atila Capovilla <capovillaatila@gmail.com>
2022-03-17 15:13:12 -0300

    Entrada Ajax para atualizar saldo produto digitado

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView
 
@@ -31,3 +32,8 @@ class ProdutoUpdate(UpdateView):
     model = Produto
     template_name = 'produto_form.html'
     form_class = ProdutoForm
+
+def produto_json(request, pk):
+    produto = Produto.objects.filter(pk=pk)
+    data = [item.to_dict_json() for item in produto]
+    return JsonResponse({'data': data})
