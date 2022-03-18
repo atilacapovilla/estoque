aaa18b2f72686344a64af1b9e5782bbb6ef32747
Atila Capovilla <capovillaatila@gmail.com>
2022-03-18 17:12:41 -0300

    Saida estoque_saida_list.html

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -5,18 +5,18 @@ from django.shortcuts import render, resolve_url
 
 from projeto.estoque.forms import EstoqueForm, EstoqueItensForm
 from projeto.estoque.models import Produto
-from .models import Estoque, EstoqueItens
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
-    objects = Estoque.objects.filter(movimento='e')
+    objects = EstoqueEntrada.objects.all()
     context = {'object_list': objects}
     return render(request, template_name, context)
 
 
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
-    obj = Estoque.objects.get(pk=pk)
+    obj = EstoqueEntrada.objects.get(pk=pk)
     context = {'object': obj}
     return render(request, template_name, context)
 
@@ -31,9 +31,9 @@ def dar_baixa_estoque(form):
 
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
-    estoque_form = Estoque()
+    estoque_form = EstoqueEntrada()
     item_estoque_formset = inlineformset_factory(
-        Estoque,
+        EstoqueEntrada,
         EstoqueItens,
         form=EstoqueItensForm, 
         extra=0,
@@ -63,3 +63,9 @@ def estoque_entrada_add(request):
     }
     return render(request, template_name, context)
 
+
+def estoque_saida_list(request):
+    template_name = 'estoque_saida_list.html'
+    objects = EstoqueSaida.objects.all()
+    context = {'object_list': objects}
+    return render(request, template_name, context)
