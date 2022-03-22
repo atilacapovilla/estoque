90f16b745e1dc1c0bf404c811fe8449b3d8e3a19
Atila Capovilla <capovillaatila@gmail.com>
2022-03-22 18:58:52 -0300

    Logout

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,6 @@
+from django import views
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,4 +8,5 @@ urlpatterns = [
     path('', include('projeto.core.urls')),
     path('produto/', include('projeto.produto.urls')),
     path('estoque/', include('projeto.estoque.urls')),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
